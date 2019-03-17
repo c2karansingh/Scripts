29ec7e2c5573c02df9c975ef00126c686f82112a
Karansingh Chauhan <31315654+c2karansingh@users.noreply.github.com>
2019-03-17 16:00:10 +0530

    Add files via upload

---
+++ GazeManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GazeManager : MonoBehaviour {
+
+    public Vector3 cameraPosition;
+    public Vector3 cameraDirection;
+    public RaycastHit hitInfo;
+    public Transform cameraTrans;
+
+
+    static public GameObject currentFocused;
+    private GameObject oldFocused;
+    //flow of getting object values/components
+
+    //get reference to the object
+    //get reference of the component
+    //fetch value
+
+    // Use this for initialization
+    void Start ()
+    {
+
+        cameraTrans = GetComponent<Transform>();
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        //Get direction and origin of the user
+
+        
+
+        cameraPosition = cameraTrans.position;
+        cameraDirection = cameraTrans.forward;
+        //cameraTrans.forward vs Vector3.forward
+
+
+
+        Debug.DrawRay(cameraPosition,cameraDirection,Color.red);
+
+        //check if it hits an object
+        if( Physics.Raycast(cameraPosition,cameraDirection,out hitInfo) )
+        {
+            // if it does, print object's name
+            //Debug.Log("Hit object ");
+            //Debug.Log(hitInfo.collider.gameObject.name );
+            currentFocused = hitInfo.collider.gameObject;
+            
+        }
+        else
+        {
+            currentFocused = null;
+        }
+
+
+
+
+        
+    }
+}
---
+++ GestureManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.WSA.Input;
+
+
+public class GestureManager : MonoBehaviour
+{
+
+    
+    
+    GestureRecognizer recognizer;
+
+    void Start()
+    {
+
+        recognizer = new GestureRecognizer();
+
+        recognizer.Tapped += tappedDetected;
+
+        recognizer.StartCapturingGestures();
+
+    }
+
+   
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            dropObject();
+        }
+    }
+   
+    void dropObject()
+    {
+        if (GazeManager.currentFocused != null)
+        {
+            Rigidbody rb = GazeManager.currentFocused.GetComponent<Rigidbody>();
+            rb.useGravity = true;
+
+        }
+
+    }
+
+    private void tappedDetected(TappedEventArgs obj)
+    {
+        dropObject();
+
+        
+
+    }
+}
